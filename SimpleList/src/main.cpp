8b3a44624050a11baae257d2bef4603589845ee7
gogi999 <g.petrovic9@gmail.com>
2026-03-11 17:25:04 +0100

    Getting user input

--- SimpleList/src/main.cpp
+++ SimpleList/src/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+using namespace std;
 
-int main()
+void print_menu(string name);
+
+int main(int arg_count, char *args[])
 {
-    std::cout << "Hello, World!" << std::endl;
+
+    if (arg_count > 1)
+    {
+        string name(args[1]);
+        print_menu(name);
+    }
+    else
+    {
+        cout << "Username not supplied... Exiting the program!!!" << endl;
+    }
     return 0;
+}
+
+void print_menu(string name)
+{
+    int choice;
+
+    cout << "***************\n";
+    cout << "1 - Print list.\n";
+    cout << "2 - Add to list.\n";
+    cout << "3 - Delete from list.\n";
+    cout << "4 - Exit.\n";
+    cout << "Enter your choice and press enter/return.\n";
+
+    cin >> choice;
+
+    if (choice == 4)
+    {
+        exit(0);
+    }
+    else
+    {
+        cout << "Sorry choice hasn't been implemented!!!\n";
+    }
 }
