b4b2fbb36e86b14679fdcc07f7932cba35279c13
gogi999 <g.petrovic9@gmail.com>
2026-03-12 12:02:05 +0100

    Vectors and functions

--- SimpleList/src/main.cpp
+++ SimpleList/src/main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+// Function prototypes
 void print_menu(string name);
+void print_list();
+void add_item();
+void delete_item();
 
+// Global variables
+vector<string> list;
+string name;
+// Main function
 int main(int arg_count, char *args[])
 {
 
     if (arg_count > 1)
     {
-        string name(args[1]);
+        name = string(args[1]);
         print_menu(name);
     }
     else
@@ -18,6 +27,7 @@ int main(int arg_count, char *args[])
     return 0;
 }
 
+// Function definitions
 void print_menu(string name)
 {
     int choice;
@@ -39,4 +49,40 @@ void print_menu(string name)
     {
         cout << "Sorry choice hasn't been implemented!!!\n";
     }
+}
+
+void add_item()
+{
+    cout << "\n\n\n\n\n\n\n\n\n";
+    cout << "*** Add Item ***\n";
+    cout << "Type in an item and press enter: ";
+    string item;
+    cin >> item;
+
+    list.push_back(item);
+    cout << "Item added to the list!!!\n\n\n\n\n";
+    cin.clear();
+
+    print_menu(name);
+}
+
+void delete_item()
+{
+    cout << "\n\n\n\n\n\n\n\n\n";
+    cout << "*** Delete Item ***\n";
+    cout << "Slecet an item number to delete\n";
+
+    if (list.size())
+    {
+        for (int i = 0; i < list.size(); i++)
+        {
+            cout << i << ": " << list[i] << endl;
+        }
+    }
+    else
+    {
+        cout << "No item in the list or to delete!!!\n";
+    }
+
+    print_menu(name);
 }
